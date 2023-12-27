2454b8ce116adbc32c415398ba44aa5ceb11bbff
Htwe Myat Cho <88830384+h-myatcho@users.noreply.github.com>
2023-12-27 18:13:55 +0700

    Update frontend

--- app.py
+++ app.py
@@ -17,23 +17,25 @@ def index():
         uploaded_file = request.files['file']
 
         # Make the API request and obtain the JSON response.
-        url = 'https://app.nanonets.com/api/v2/OCR/Model/4b02ca67-557a-4cd3-af8e-a19dcc58898c/LabelUrls/?async=false'
+        url = 'https://app.nanonets.com/api/v2/OCR/Model/f8f086b3-7480-4e2b-bef7-0e5e8bd08479/LabelUrls/?async=false'
         headers = {'accept': 'application/x-www-form-urlencoded'}
         files = {'file': (uploaded_file.filename, uploaded_file.stream, uploaded_file.mimetype)}
-        auth = requests.auth.HTTPBasicAuth('dba71e7c-7d8b-11ee-998f-7efeff8449b1', '')
+        auth = requests.auth.HTTPBasicAuth('4e121e69-8f9e-11ee-8a1f-4a1e4b95b6ce', '')
 
         apiResponse = requests.post(url, headers=headers, auth=auth, files=files)
-
+        # print("API Response:", apiResponse.text)
+        
         # Get json raw object responsed from Nanonet.
         apiResponseJson = apiResponse.json()
+        print("API Response JSON:", apiResponseJson)
 
         # Filter to get only table cells from json object.
         filteredJson = apiResponseJson.get("result", [])[0].get("prediction", [])
 
-    # Testing with sample response first before api response.
-    with open('data/sampleResponse.json', 'r') as file:
-        sampleResponseJson = json.load(file)
-    filteredJson = sampleResponseJson.get("result", [])[0].get("prediction", [])
+    # # Testing with sample response first before api response.
+    # with open('data/sampleResponse.json', 'r') as file:
+    #     sampleResponseJson = json.load(file)
+    # filteredJson = sampleResponseJson.get("result", [])[0].get("prediction", [])
 
     # call buildReport function from model class to build report if the data is present.
     if filteredJson is not None:
@@ -43,6 +45,13 @@ def index():
 
     return render_template('index.html', response_text=reportJson)
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port='5001')
